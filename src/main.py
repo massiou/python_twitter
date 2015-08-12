ad747d22e10f319b68f42ad150da64a934e14d33
Matthieu Velay <matthieu.velay@parrot.com>
2015-08-12 11:28:24 +0200

    [DEV] Delete old friends

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import re
 import sys
 import json
 import time
-
+import random
 # Tierce imports
 import twitter
 
@@ -28,9 +28,12 @@ logger = logging.getLogger('tweet')
 logger.setLevel(logging.DEBUG)
 # Build formatter for each handler
 formatter_file = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
-formatter_console = logging.Formatter(yellow('[%(asctime)s]') + \
-                                      blue('[%(levelname)s]') + \
+if sys.stdout.isatty():
+    formatter_console = logging.Formatter(yellow('[%(asctime)s]') +
+                                      blue('[%(levelname)s]') +
                                       white(' %(message)s'))
+else:
+    formatter_console = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
 
 # Set handlers filesize is < 1Mo
 file_handler = RotatingFileHandler('tweet.log', 'a', 1000000, 1)
@@ -59,10 +62,11 @@ class TwitterInstance(object):
 
     def __init__(self, keys):
         '''init my instance twitter'''
-        self._instance = twitter.Api(consumer_key=keys['consumer_key'], \
-                                    consumer_secret=keys['consumer_secret'], \
-                                    access_token_key=keys['access_token_key'], \
-                                    access_token_secret=keys['access_token_secret'])
+        print keys
+        self._instance = twitter.Api(consumer_key=str(keys['consumer_key']), \
+                                    consumer_secret=str(keys['consumer_secret']), \
+                                    access_token_key=str(keys['access_token_key']), \
+                                    access_token_secret=str(keys['access_token_secret']))
 
         self._timeline = self._instance.GetHomeTimeline()
         self._friendIDs = self.instance.GetFriendIDs()
@@ -96,9 +100,9 @@ class TwitterInstance(object):
                    if all([re.search(word, status.text) for word in words_list]) ]
         return tweets
 
-    def get_all_tweets_given_words(self, words_list, date_from=None):
+    def get_all_tweets_given_words(self, words_list, lang='fr', count=300, result_type='recent', date_from=None):
         ''' get all tweets regarding to word list'''
-        return self.instance.GetSearch(term=words_list, lang='fr', result_type='recent', count=50, until=date_from)        
+        return self.instance.GetSearch(term=words_list, lang=lang, result_type=result_type, count=count, until=date_from)
 
     def destroy_friendship(self, friend_id):
         logger.info('Destroy friendship: %d' % friend_id)
@@ -109,6 +113,15 @@ class TwitterInstance(object):
         if len(self.friendIDs) - number < 50:
             raise TooMuchFriendsToDelete
         friends_to_delete = self.friendIDs[:number]
+        for friend in friends_to_delete:
+            self.destroy_friendship(friend)
+
+    def destroy_old_friends(self, from_number):
+        ''' destroy friends from from_number id'''
+        if from_number < 1000:
+            raise TooMuchFriendsToDelete
+        friends_to_delete = self.friendIDs[from_number:]
+        logger.info('destroy OLD friends from %d' % from_number)
         for friend in friends_to_delete:
             self.destroy_friendship(friend)
 
@@ -124,20 +137,34 @@ if __name__ == '__main__':
     # Globals relied to account
     ACCOUNT_GLOBALS = get_account_globals('./twitter.json')
     KEYS = ACCOUNT_GLOBALS['KEYS']
+    print KEYS
     BLACKLIST_ID = ACCOUNT_GLOBALS['BLACKLIST_ID']
     FORBIDDEN_WORDS = ACCOUNT_GLOBALS['FORBIDDEN_WORDS']
     # Create twitter instance
     mv_twitter = TwitterInstance(KEYS)
 
+    # Ensure number of friends is correct (twitter limit is 2000)
+
+    if len(mv_twitter.friendIDs) > 1950:
+        mv_twitter.destroy_old_friends(1949)
+
     # get tweets from all tweets search
     date_from = time.strftime('%Y-%m-%d')
-    #tweets_to_rt = mv_twitter.get_all_tweets_given_words(["rt+follow"], date_from)
-    #tweets_to_rt += mv_twitter.get_all_tweets_given_words(["follow+rt"], date_from)
-    tweets_to_rt = mv_twitter.get_all_tweets_given_words(["Concours", "follow"])
-    tweets_to_rt += mv_twitter.get_all_tweets_given_words(["Gagner", "follow"])
-    for tweet in tweets_to_rt:
+
+    tweets_to_rt = mv_twitter.get_all_tweets_given_words(["Concours", "follow"], lang='fr', count=20, result_type='popular')
+    tweets_to_rt += mv_twitter.get_all_tweets_given_words(["Concours", "follow"], lang='fr', count=20, result_type='recent')
+    tweets_to_rt += mv_twitter.get_all_tweets_given_words(["Gagner", "follow"], lang='fr', count=20, result_type='recent')
+    tweets_to_rt += mv_twitter.get_all_tweets_given_words(["Gagner", "follow"], lang='fr', count=20, result_type='popular')
+    #tweets_to_rt += mv_twitter.get_all_tweets_given_words(["win", "follow"], lang='en', count=100, result_type='popular')
+    #tweets_to_rt += mv_twitter.get_all_tweets_given_words(["win", "follow"], lang='en', count=100, result_type='recent')
+
+    logger.info("Length Tweets to RT: %d", len(tweets_to_rt))
+    for index, tweet in enumerate(tweets_to_rt):
         # Exclude fordidden words
-        if not any(word for word in FORBIDDEN_WORDS if word in tweet.text):
+        logger.info('%d tweet index', index)
+        text = tweet.text.lower()
+        if not any(word for word in FORBIDDEN_WORDS if word in text) \
+           and not (text.startswith('rt ') or text.startswith('rt@')):
             if tweet.retweeted_status:
                 # the current tweet is a RT
                 rt_dict = json.loads(str(tweet.retweeted_status))
@@ -149,18 +176,20 @@ if __name__ == '__main__':
                 tweet_user = json.loads(str(tweet.GetUser()))
                 user_id = tweet_user['id']
 
-            logger.info('%s: %s', tweet_id, tweet.text)
-        
+            logger.info('%s: %s', tweet_id, tweet.text[:25])
+
             if user_id not in BLACKLIST_ID:
                 try:
                 # RT
+                    time_to_wait = random.randint(5, 10)
+                    print 'wait: %d' % time_to_wait
+                    time.sleep(time_to_wait)
                     mv_twitter.instance.PostRetweet(tweet_id)
                 except twitter.TwitterError as msg:
                     logger.error(msg)
                 except Exception as msg:
                     raise msg
                 # Follow
-
                 logger.info('Try to follow:%s' % str(user_id))
                 if user_id not in mv_twitter.friendIDs:
                     try:
@@ -170,10 +199,11 @@ if __name__ == '__main__':
                     except Exception as msg:
                         raise msg
                 else:
-                    logger.info('user id already in friends list : %d' % user_id)
+                    logger.info('user id already in friends list : %d', user_id)
             else:
-                logger.warning('user is in blacklist: %d' % user_id)
+                logger.warning('user is in blacklist: %d',  user_id)
         else:
-            logger.info('find a forbidden word in %s' % tweet.text)
-    
+            logger.info('%d find a forbidden word in %s', index, tweet.text)
+
+
 
